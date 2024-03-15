f8b8a556ebb8f5ddec4c748797b809f1d3a531f2
Harsh-scs <harsh_shah@codenticsoftware.com>
2024-03-15 18:15:47 +0530

    New

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import CustomeTabBar from './app/components/CustomTabbar'
 
 export default function App() {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+
+      <CustomeTabBar/>
+    
   )
 }
