9767aea8974a89543e7c249da9b110eaf8a37c0b
Harsh-scs <harsh_shah@codenticsoftware.com>
2024-03-15 11:24:45 +0530

    New

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { fonts } from './app/theme'
 
 export default function App() {
   return (
     <View>
-      <Text>App</Text>
+      <Text style={{fontFamily:fonts.PoppinsBold}}>App</Text>
     </View>
   )
 }
