be4c7c3c7868e695fa974c2d840d371c4b56211c
Priyanka <ps0665818@gmail.com>
2024-03-15 15:52:44 +0530

    new

--- App.tsx
+++ App.tsx
@@ -1,16 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-<<<<<<< HEAD
-import { images } from './app/theme'
-import Customheader from './app/components/CustomHeader'
-=======
->>>>>>> 642aaee06020cc123afbc4b8eefcf830dc5d96ed
 
 export default function App() {
   return (
     <View>
       
-      <Customheader type='first'/>
+      <Text>
+        Hey
+      </Text>
     </View>
   )
 }
