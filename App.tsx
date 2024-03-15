6b5a2d1f2a8c2c8eb493a71745f9c247e0618eda
Priyanka <ps0665818@gmail.com>
2024-03-15 12:56:12 +0530

    new

--- App.tsx
+++ App.tsx
@@ -1,10 +1,30 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import OrangeMulticity from './app/theme/images/svg/Orangemulticity'
+import Beach from './app/theme/images/svg/Beach'
+import Biggraystar from './app/theme/images/svg/Biggraystar'
+import Food from './app/theme/images/svg/Food'
+import Grayaccount from './app/theme/images/svg/Grayaccount'
+import Graycross from './app/theme/images/svg/Graycross'
+import Grayhome from './app/theme/images/svg/Grayhome'
+import Graynotification from './app/theme/images/svg/Graynotification'
+import Graysinglestar from './app/theme/images/svg/Graysinglestar'
+import Graysuitcase from './app/theme/images/svg/Graysuitcase'
+import Halfgraystar from './app/theme/images/svg/Halfgraystar'
+import Homesearch from './app/theme/images/svg/Homesearch'
+import Multicity from './app/theme/images/svg/Multicity'
+import Orangeaccount from './app/theme/images/svg/Orangeaccount'
+import Orangebus from './app/theme/images/svg/Orangebus'
+import Orangecab from './app/theme/images/svg/Orangecab'
+import Orangehome from './app/theme/images/svg/Orangehome'
+import Orangehotel from './app/theme/images/svg/Orangehotel'
 
 export default function App() {
   return (
     <View>
       <Text>App</Text>
+     
+
     </View>
   )
 }
