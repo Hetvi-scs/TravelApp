8727235c6f4073eaa435d0392472c4f73f07daca
Harsh-scs <harsh_shah@codenticsoftware.com>
2024-03-18 12:20:00 +0530

    New

--- App.tsx
+++ App.tsx
@@ -1,10 +1,9 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import CustomeHotel from './app/components/CustomeHotel'
 
 export default function App() {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <CustomeHotel></CustomeHotel>
   )
 }
