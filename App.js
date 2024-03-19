42f31946fcc0025ac65927fac39f42dd009ed0f2
KUNJAN KACHHIA <kunjan_kachhia@codenticsoftware.com>
2024-03-19 15:01:21 +0530

    splashscreen  done

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import {View, Text} from 'react-native';
 import React from 'react';
 import SplashScreen from './app/screens/SplashScreen';
 import {color} from './app/theme';
+import {NavigationContainer} from '@react-navigation/native';
+import Bottomtab from './app/navigation/bottomTabNavigation';
 
 export default function Home() {
   return (
-    <View style={{flex: 1, backgroundColor: color.white}}>
-      <SplashScreen />
-    </View>
+    <NavigationContainer>
+      <Bottomtab />
+    </NavigationContainer>
   );
 }
--- app/screens/Home/index.js
+++ app/screens/Home/index.js
@@ -1,10 +1,20 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, Text} from 'react-native';
+import React from 'react';
+import {size} from '../../theme';
 
 export default function Home() {
   return (
     <View>
-      <Text>Home</Text>
+      {/* <StatusBar translucent backgroundColor={'transparent'}></StatusBar> */}
+      <View
+        style={{
+          width: size.deviceWidth,
+          height: size.moderateScale(410),
+          borderBottomLeftRadius: size.moderateScale(48),
+          borderBottomRightRadius: size.moderateScale(48),
+        }}>
+        <Text>hiiiiiii</Text>
+      </View>
     </View>
-  )
+  );
 }
