1519d6819acf020833204813043cf9ea4cd0e225
KUNJAN KACHHIA <kunjan_kachhia@codenticsoftware.com>
2024-03-19 14:26:36 +0530

    splashscreen  done

--- app/screens/SplashScreen/index.js
+++ app/screens/SplashScreen/index.js
@@ -1,18 +1,45 @@
-import {View, Text} from 'react-native';
+import {View, Text, Image, TouchableOpacity} from 'react-native';
 import React from 'react';
-import {size} from '../../theme';
+import {
+  IcGreenActiveStar,
+  IcRightsideArrow,
+  IcRightwhitearrow,
+  IcWhitetower,
+  color,
+  fontSize,
+  fonts,
+  images,
+  size,
+} from '../../theme';
+import Rightwhitearrow from '../../theme/images/svg/Rightwhitearrow';
 
 export default function SplashScreen() {
   return (
     <View>
       <View
         style={{
+          width: size.deviceWidth,
           height: size.moderateScale(400),
-          backgroundColor: 'red',
           borderBottomLeftRadius: size.moderateScale(48),
           borderBottomRightRadius: size.moderateScale(48),
         }}>
-        <View></View>
+        <View
+          style={{
+            position: 'absolute',
+            zIndex: 2,
+            margin: size.moderateScale(20),
+          }}>
+          <IcWhitetower />
+        </View>
+        <Image
+          source={images.hotel}
+          style={{
+            width: size.deviceWidth,
+            height: size.moderateScale(400),
+            borderBottomLeftRadius: size.moderateScale(48),
+            borderBottomRightRadius: size.moderateScale(48),
+          }}
+        />
       </View>
       <View
         style={{
@@ -22,26 +49,47 @@ export default function SplashScreen() {
         }}>
         <View
           style={{
-            backgroundColor: 'blue',
-            height: size.moderateScale(121),
+            height: size.moderateScale(150),
+            marginHorizontal: size.moderateScale(25),
           }}>
           <View>
-            <Text>Tripify</Text>
+            <Text
+              style={{
+                fontSize: fontSize.big,
+                fontFamily: fonts.PoppinsMedium,
+                color: color.lightblack,
+                textAlign: 'center',
+              }}>
+              Tripify
+            </Text>
           </View>
           <View>
-            <Text>
+            <Text
+              style={{
+                fontSize: fontSize.medium,
+                fontFamily: fonts.PoppinsLight,
+                color: color.semiblack,
+                textAlign: 'center',
+              }}>
               The ultimate travel app for easy multi-city adventures. Plan,
               book, and explore hassle-free.
             </Text>
           </View>
         </View>
-        <View
-          style={{
-            backgroundColor: 'blue',
-            height: size.moderateScale(72),
-            width: size.moderateScale(72),
-            borderRadius: size.moderateScale(72),
-          }}></View>
+        <TouchableOpacity onPress={() => {}}>
+          <View
+            style={{
+              marginVertical: size.moderateScale(10),
+              backgroundColor: color.orange,
+              height: size.moderateScale(72),
+              width: size.moderateScale(72),
+              borderRadius: size.moderateScale(72),
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}>
+            <IcRightwhitearrow />
+          </View>
+        </TouchableOpacity>
       </View>
     </View>
   );
--- app/theme/images/svg/Whitetower.js
+++ app/theme/images/svg/Whitetower.js
@@ -1,17 +1,16 @@
-import * as React from "react"
-import Svg, { SvgProps, Path } from "react-native-svg"
-const Whitetower = (props) => (
+import * as React from 'react';
+import Svg, {SvgProps, Path} from 'react-native-svg';
+const Whitetower = props => (
   <Svg
     xmlns="http://www.w3.org/2000/svg"
-    width={24}
-    height={24}
+    width={35}
+    height={35}
     fill="none"
-    {...props}
-  >
+    {...props}>
     <Path
       fill="#fff"
       d="m.179 35.503 11.33-7.456 11.323 7.456L11.508.733.178 35.502Z"
     />
   </Svg>
-)
-export default Whitetower
+);
+export default Whitetower;
