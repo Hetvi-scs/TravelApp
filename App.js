85530c5e3c7bc09a9c2b8ad2bfafdf7e6d20577c
Priyanka <ps0665818@gmail.com>
2024-03-20 11:06:19 +0530

    new

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, Text} from 'react-native';
+import React from 'react';
+import SplashScreen from './app/screens/SplashScreen';
+import {IcBank, color} from './app/theme';
+import Customheader from './app/components/CustomHeader';
+import Upcomingcard from './app/components/Upcomingcard';
+import Profile from './app/screens/Profile';
 
-export default function App() {
+import {NavigationContainer} from '@react-navigation/native';
+import Bottomtab from './app/navigation/bottomTabNavigation';
+
+export default function Home() {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
-  )
+    <NavigationContainer>
+      <Bottomtab />
+    </NavigationContainer>
+  );
 }
