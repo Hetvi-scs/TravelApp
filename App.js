0fbdefbc296a0f4146083953509b83665d6194c5
unknown <hetvi_sundarkar@codenticsoftware.com>
2024-03-20 11:59:12 +0530

    2nd card

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import SelectPaymentMode from './app/screens/SelectPaymentMode.js'
+import {View, Text} from 'react-native';
+import React from 'react';
+import SplashScreen from './app/screens/SplashScreen';
+import {IcBank, color} from './app/theme';
+import Customheader from './app/components/CustomHeader';
+import Upcomingcard from './app/components/Upcomingcard';
+import Profile from './app/screens/Profile';
+
+import {NavigationContainer} from '@react-navigation/native';
+import Bottomtab from './app/navigation/bottomTabNavigation';
 
 export default function App() {
   return (
-    <View>
-    <SelectPaymentMode/>
-    </View>
-  )
+    <NavigationContainer>
+      <Bottomtab />
+    </NavigationContainer>
+  );
 }
