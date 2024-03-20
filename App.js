7e497a2b41a9bb1e12f84e7514e1d1b3cc092f2d
Harsh-scs <harsh_shah@codenticsoftware.com>
2024-03-20 17:45:26 +0530

    Filter done

--- App.js
+++ App.js
@@ -2,10 +2,14 @@ import {View, Text} from 'react-native';
 import React from 'react';
 
 import Filter2 from './app/screens/Fliter2/index.js';
+import { NavigationContainer } from '@react-navigation/native';
+import MainStck from './app/navigation/mainStackNavigation/index.js';
 
 export default function Home() {
   return (
-   <Filter2/>
+   <NavigationContainer>
+    <MainStck/>
+   </NavigationContainer>
    
     
   );
--- app/navigation/FlightBookingTop/styles.js
+++ app/navigation/FlightBookingTop/styles.js
@@ -74,3 +74,44 @@ export const btnview = () => ({
 });
 
 export const btnview2 = () => ({width: '46%'});
+
+
+export const radio1 = () => ({
+  // flexDirection: 'row',
+  // alignItems: 'center',
+  width: 24,
+  height: 24,
+  borderRadius: 24,
+  borderWidth: 2.5,
+  justifyContent: 'center',
+  alignItems: 'center',
+  marginTop: 10,
+  
+});
+export const maincheckboxview = () => ({
+  flexDirection: 'row',
+
+  marginHorizontal: size.moderateScale(25),
+  marginTop: size.moderateScale(16),
+
+  alignItems: 'center',
+});
+export const maincheckboxview1 = () => ({
+  flexDirection: 'row',
+
+  marginTop: size.moderateScale(16),
+
+  alignItems: 'center',
+});
+
+export const maincheckboxview2 = () => ({
+    flexDirection: 'row',
+  
+    marginTop: size.moderateScale(16),
+  
+    alignItems: 'center',
+  });
+export const labels = () => ({
+  fontSize: fontSize.small,
+color:color.black
+});
